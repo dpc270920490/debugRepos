4f064ca3dc56075b16b9844bd6a60fad42251472
dpc270920490 <270920490@qq.com>
2018-07-10 14:57:53 +0800

    测试

---
+++ src/main/java/cn/qzt/pojo/User.java
@@ -0,0 +1,29 @@
+package cn.qzt.pojo;
+
+/**
+ * Title:  debug
+ * Description:  SSD
+ *
+ * @author dc
+ * @date2018/7/1014:55
+ */
+public class User {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
